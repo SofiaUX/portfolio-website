84b57d709241dc646bd8ae785005fbc0cd109189
SofiaUX <sofoklis2008@gmail.com>
2018-12-11 15:19:48 +0100

    Add js to home images

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("Welcome to SofiaUX Portfolio Site");
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "350px";
+    document.getElementById("main").style.marginLeft = "350px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
