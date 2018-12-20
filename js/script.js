509d01bf45742343cd5435223f6c9669ec870249
SofiaUX <sofoklis2008@gmail.com>
2018-12-20 10:00:57 +0100

    User testing improvements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log("Welcome to SofiaUX Portfolio Site");
 
-// Sidebar effect
+
 function openNav() {
     document.getElementById("mySidebar").style.width = "350px";
     document.getElementById("main").style.marginLeft = "350px";
@@ -29,27 +29,26 @@ $(document).ready(function() {
 
 
 
-
-
   // Slideshow effect at book illustration 1
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+  function currentDiv(n) {
+  showDivs(slideIndex = n);
 }
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("myPages");
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
   x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
 }
-
 
 
 
@@ -65,10 +64,11 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("Slides");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
+}
