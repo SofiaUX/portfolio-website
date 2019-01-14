87e9ffbb632ff04e98b4010c010e3a686ae78032
SofiaUX <sofoklis2008@gmail.com>
2019-01-14 19:50:49 +0100

    Remove css file

--- js/script.js
+++ js/script.js
@@ -69,7 +69,9 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  if (x[slideIndex-1]) {
+      x[slideIndex-1].style.display = "block";
+  }
 }
 
 
@@ -87,7 +89,9 @@ function carousel() {
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
+  if (x[slideIndex-1]){
+      x[myIndex-1].style.display = "block";
+  }
   setTimeout(carousel, 5000); // Change image every 2 seconds
 }
 
@@ -101,18 +105,23 @@ var modal = document.getElementById('myModal');
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+if (img ){
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
 }
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+if ( span){
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
 }
 
 
@@ -134,7 +143,11 @@ function showSlides() {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  if (slides[slideIndex-1]){
+     slides[slideIndex-1].style.display = "block";
+  }
+  if (dots[slideIndex-1]){
+     dots[slideIndex-1].className += " active";
+  }
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
